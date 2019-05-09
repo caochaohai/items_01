cc835a900f44b85fb92ddc386d0f7044435b4e6c
caochaohai <947220846@qq.com>
2019-05-09 16:18:11 +0800

    跟新driver,备注了代码

--- base/base_action.py
+++ base/base_action.py
@@ -2,26 +2,26 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BaseAction:
-
+    #
     def __init__(self, driver):
         self.driver = driver
-
+    #使用显示等待查找元素,并返回元素
     def find_element(self, feature, timeout=10, poll=1):
         by, value = feature
         return WebDriverWait(self.driver, timeout, poll).until(lambda x:x.find_element(by, value))
-
+    #点击
     def click(self, feature):
         self.find_element(feature).click()
-
+    #输入
     def input(self, feature, content):
         self.clear(feature)
         self.find_element(feature).send_keys(content)
-
+    #清空
     def clear(self, feature):
         self.find_element(feature).clear()
-
+    #返回
     def press_back(self):
         self.driver.press_keycode(4)
-
+    #确认
     def press_enter(self):
         self.driver.press_keycode(66)
--- base/base_driver.py
+++ base/base_driver.py
@@ -8,7 +8,10 @@ def init_driver():
     desired_caps['platformVersion'] = '5.1'
     desired_caps['deviceName'] = '192.168.56.101:5555'
     # app信息
-    desired_caps['appPackage'] = 'com.android.contacts'
-    desired_caps['appActivity'] = '.activities.PeopleActivity '
+    desired_caps['appPackage'] = 'com.tpshop.malls'
+    desired_caps['appActivity'] = '.SPMainActivity '
     desired_caps['noReset'] = True
+    desired_caps['automationName'] = 'Uiautomator2'
     return webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+    
